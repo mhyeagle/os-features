b958b5594305303b5f16e2add5df165c10f42cf3
luna <tianyashuibin@163.com>
2017-04-22 11:45:09 +0800

    socket fd

--- file_system/open_file_number.c
+++ file_system/open_file_number.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 int main() {
     FILE* f1 = fopen("/usr/include/aio.h", "r");
@@ -7,6 +11,9 @@ int main() {
 
     FILE* f2 = fopen("/usr/include/elf.h", "r");
     printf("f2 file descriptor: %d\n", f2->_fileno);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("sockfd number: %d\n", sockfd);
 
     return 0;
 }
